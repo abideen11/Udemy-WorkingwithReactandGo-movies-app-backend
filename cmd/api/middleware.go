a6e6507c2d7b468f0166e31ca86245ef57db91b4
Abideen Kareem <abideenkareem17@gmail.com>
2022-02-05 19:35:33 -0600

    Configure more authorization

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,78 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"github.com/pascaldekloe/jwt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("Access-Control-Allow-Origin", "*")
 		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		rw.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+
+		next.ServeHTTP(rw, r)
+	})
+}
+
+func (app *application) checkToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Vary", "Authorization")
+		// rw.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		// rw.Header().Add("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+
+		authHeader := r.Header.Get("Authorization")
+
+		if authHeader == "" {
+			// could set an anonymous user
+		}
+
+		headerParts := strings.Split(authHeader, " ")
+		if len(headerParts) != 2 {
+			app.errorJSON(rw, errors.New("invalid auth header"))
+			return
+		}
+
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(rw, errors.New("unauthorized - no bearer"))
+			return
+		}
+
+		token := headerParts[1]
+
+		claims, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+		if err != nil {
+			app.errorJSON(rw, errors.New("unauthorized - failed hmac check"), http.StatusForbidden)
+			return
+		}
+
+		if !claims.Valid(time.Now()) {
+			app.errorJSON(rw, errors.New("unauthorized - token expired"), http.StatusForbidden)
+			return
+		}
+
+		if !claims.AcceptAudience("mydomain.com") {
+			app.errorJSON(rw, errors.New("unauthorized - invalid audience"), http.StatusForbidden)
+			return
+		}
+
+		if claims.Issuer != "mydomain.com" {
+			app.errorJSON(rw, errors.New("unauthorized - invalid issuer"), http.StatusForbidden)
+			return
+		}
+
+		userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+		if err != nil {
+			app.errorJSON(rw, errors.New("unauthorized"), http.StatusForbidden)
+			return
+		}
+
+		log.Println("Valid user: ", userID)
 
 		next.ServeHTTP(rw, r)
 	})
